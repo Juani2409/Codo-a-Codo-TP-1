b48db174e3b6c73ad9c3fc0aa4321dd27d190ffd
Juan Ignacio Grodz <jigrodz@gmail.com>
2023-10-31 16:36:29 -0300

    Se agregó nueva alerta para que completen todos los campos

--- tickets.js
+++ tickets.js
@@ -1,4 +1,3 @@
-
 function calcularTotal() {
     const cantidad = document.getElementById("cantidadInput").value;
     const tipo = document.getElementById("tipoSelect").value;
@@ -8,21 +7,15 @@ function calcularTotal() {
     if (tipo === "estudiante") {
         total -= total * 0.8; // Resta el 80% si el tipo es "estudiante"
     }
-    else if(tipo === "junior") {
+    else if (tipo === "junior") {
         total -= total * 0.5; 
-    }else{
+    } else {
         total -= total * 0.15;
     }
 
     document.getElementById("totalPagar").textContent = `Total a Pagar: $${total.toFixed(2)}`;
 }
 
-
-
-
-
-
-// Función para mostrar un resumen en una ventana emergente
 function mostrarResumen() {
     const nombre = document.querySelector('input[placeholder="Nombre"]').value;
     const apellido = document.querySelector('input[placeholder="Apellido"]').value;
@@ -33,9 +26,21 @@ function mostrarResumen() {
 
     const resumen = `Nombre: ${nombre}\nApellido: ${apellido}\nCorreo: ${correo}\nCantidad: ${cantidad}\nCategoria: ${categoria}\nTotal a Pagar: ${total}`;
 
-    // Muestra el resumen en una ventana emergente (popup)
     alert(resumen);
 }
 
-// Asigna el evento al botón "Resumen"
-document.getElementById('resumenButton').addEventListener('click', mostrarResumen);
+function verificarRequisitos() {
+    const nombre = document.querySelector('input[placeholder="Nombre"]').value;
+    const apellido = document.querySelector('input[placeholder="Apellido"]').value;
+    const correo = document.querySelector('input[placeholder="Correo"]').value;
+    const cantidad = document.getElementById("cantidadInput").value;
+    const categoria = document.getElementById("tipoSelect").value;
+
+    if (nombre.trim() === '' || apellido.trim() === '' || correo.trim() === '' || cantidad.trim() === '' || categoria.trim() === '') {
+        alert('Por favor, complete todos los campos.');
+    } else {
+        mostrarResumen();
+    }
+}
+
+document.getElementById('resumenButton').addEventListener('click', verificarRequisitos);
